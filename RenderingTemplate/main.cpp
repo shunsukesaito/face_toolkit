7826c073480e7b88ca49854680b9821934985004
Shunsuke Saito <shunsuke.saito16@gmail.com>
2017-06-10 17:37:54 -0400

    Initial Commit

---
+++ RenderingTemplate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RenderingTemplate
+//
+//  Created by Shunsuke Saito on 6/10/17.
+//  Copyright © 2017 Shunsuke Saito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
