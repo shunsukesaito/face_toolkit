0747a8eaf6ce2cd016f8c5f5df5aafa2c0409470
Shunsuke Saito <shunsuke.saito16@gmail.com>
2018-04-05 12:54:37 -0700

    bug fixed

--- application/face-view/run.sh
+++ application/face-view/run.sh
@@ -1,1 +1,3 @@
-./batch_f2f --facemodel bv --renderer f2fp2dbg --cam_w 2444 --cam_h 1718 --camera_fov 30 --win_w 611 --win_h 430 --loader /home/ICT2000/ssaito/Documents/FaceData/CFD/list.txt
+#./batch_f2f --facemodel bv --renderer f2fp2dbg --cam_w 2444 --cam_h 1718 --camera_fov 30 --win_w 611 --win_h 430 --loader /home/ICT2000/ssaito/Documents/FaceData/CFD/list.txt
+
+./faceview --facemode bv --renderer f2f --cam_w 256 --cam_h 256 --camera_fov 30 --win_w 512 --win_h 512 --fd_path /home/ICT2000/ssaito/ckpt/uvmofa_all_v3/scitech_%06d.txt --fd_begin_id 0 --fd_end_id 503 --mode preview
