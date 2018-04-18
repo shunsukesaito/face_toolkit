8016e9fdfe3206a08a172464738946a5fafa1ba2
Shunsuke Saito <shunsuke.saito16@gmail.com>
2018-04-17 18:08:18 -0700

    minor modification

--- ModuleGate.cmake
+++ ModuleGate.cmake
@@ -1,6 +1,6 @@
 # entry point of module structure
-if(NOT PINSCREEN_ROOT)
-	get_filename_component(PINSCREEN_ROOT ${CMAKE_CURRENT_LIST_DIR} ABSOLUTE)
-	get_filename_component(MODULE_MANAGER_ROOT ${PINSCREEN_ROOT}/miscellany/module-manager ABSOLUTE)
-	include(${MODULE_MANAGER_ROOT}/PinscreenModule.cmake)
+if(NOT FACETOOLKIT_ROOT)
+	get_filename_component(FACETOOLKIT_ROOT ${CMAKE_CURRENT_LIST_DIR} ABSOLUTE)
+	get_filename_component(MODULE_MANAGER_ROOT ${FACETOOLKIT_ROOT}/miscellany/module-manager ABSOLUTE)
+	include(${MODULE_MANAGER_ROOT}/ToolkitModule.cmake)
 endif()
--- miscellany/module-manager/ToolkitModule.cmake
+++ miscellany/module-manager/ToolkitModule.cmake
@@ -1,8 +1,8 @@
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  set (PINSCREEN_OS_SUFFIX "mac")
+  set (FACETOOLKIT_OS_SUFFIX "mac")
 endif()
 if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-  set (PINSCREEN_OS_SUFFIX "linux")
+  set (FACETOOLKIT_OS_SUFFIX "linux")
 endif()
 
 # When this is on, nothing internal or the ones with install/ will get built.
@@ -26,10 +26,10 @@ macro(returnExternals EXTERNALS)
 endmacro(returnExternals)
 
 macro(findModule MODULE)
-  get_filename_component(_modulePath "${PINSCREEN_ROOT}/${MODULE}" ABSOLUTE)
+  get_filename_component(_modulePath "${FACETOOLKIT_ROOT}/${MODULE}" ABSOLUTE)
   get_filename_component(_moduleName "${MODULE}" NAME)
   get_filename_component(_moduleFolder "${MODULE}" DIRECTORY)
-  if(NOT TARGET PSM::${_moduleName})
+  if(NOT TARGET FTM::${_moduleName})
     add_subdirectory("${_modulePath}" "${CMAKE_BINARY_DIR}/modules/${MODULE}" EXCLUDE_FROM_ALL)
     if(TARGET ${_moduleName}) # cleanup in IDE
       set_property(TARGET ${_moduleName} PROPERTY FOLDER "${_moduleFolder}")
@@ -39,18 +39,18 @@ endmacro(findModule)
 
 
 
-macro(PinscreenModuleBegin BASE_MODULE_NAME)
-  if(TARGET PSM::${BASE_MODULE_NAME})
+macro(FaceToolkitModuleBegin BASE_MODULE_NAME)
+  if(TARGET FTM::${BASE_MODULE_NAME})
     return()
   endif()
   set(_this_EXTERNAL_MODULES ${EXTERNAL_MODULES}) # backup
   set(EXTERNAL_MODULES "") # now list externals of children
 
   set(multiValueArgs DEPENDS DEFINITIONS LANGUAGES)
-  cmake_parse_arguments(PSC_MODULE "" "" "${multiValueArgs}" ${ARGN} )
+  cmake_parse_arguments(FT_MODULE "" "" "${multiValueArgs}" ${ARGN} )
 
   # add every child modules first
-  foreach(_module ${PSC_MODULE_DEPENDS})
+  foreach(_module ${FT_MODULE_DEPENDS})
     findModule(${_module})
   endforeach(_module)
 
@@ -85,30 +85,30 @@ macro(PinscreenModuleBegin BASE_MODULE_NAME)
   endif()
   message("configuring ${BASE_MODULE_NAME}")
 
-  if(PSC_MODULE_LANGUAGES)
-    project(${BASE_MODULE_NAME} LANGUAGES ${PSC_MODULE_LANGUAGES})
+  if(FT_MODULE_LANGUAGES)
+    project(${BASE_MODULE_NAME} LANGUAGES ${FT_MODULE_LANGUAGES})
   else()
     project(${BASE_MODULE_NAME})
   endif()
   include("${MODULE_MANAGER_ROOT}/ccache/setup.cmake")
   # here follows the actual code
-endmacro(PinscreenModuleBegin)
+endmacro(FaceToolkitModuleBegin)
 
 
-macro(PinscreenModulePrebuilt BASE_MODULE_NAME)
-  if(TARGET PSM::${BASE_MODULE_NAME})
+macro(FaceToolkitModulePrebuilt BASE_MODULE_NAME)
+  if(TARGET FTM::${BASE_MODULE_NAME})
     return()
   endif()
   set(_this_EXTERNAL_MODULES ${EXTERNAL_MODULES}) # backup
   set(EXTERNAL_MODULES "") # now list externals of children
 
   set(multiValueArgs DEPENDS DEFINITIONS)
-  cmake_parse_arguments(PSC_MODULE "" "" "${multiValueArgs}" ${ARGN} )
+  cmake_parse_arguments(FT_MODULE "" "" "${multiValueArgs}" ${ARGN} )
 
   # ACTUAL_BUILDING should be added only by itself. If it's OFF we handle dependencies
   if(NOT ACTUAL_BUILDING)
     # add every child modules first
-    foreach(_module ${PSC_MODULE_DEPENDS})
+    foreach(_module ${FT_MODULE_DEPENDS})
       findModule(${_module})
     endforeach(_module)
 
@@ -124,7 +124,7 @@ macro(PinscreenModulePrebuilt BASE_MODULE_NAME)
       ExternalProject_Add (${BASE_MODULE_NAME}
         DEPENDS ${EXTERNAL_MODULES}
         SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}"
-        CMAKE_ARGS -DACTUAL_BUILDING=ON "-DCMAKE_INSTALL_PREFIX=${MODULE_INSTALL_PATH}" "-DPINSCREEN_PLATFORM=${PINSCREEN_PLATFORM}"
+        CMAKE_ARGS -DACTUAL_BUILDING=ON "-DCMAKE_INSTALL_PREFIX=${MODULE_INSTALL_PATH}" "-DFaceToolkit_PLATFORM=${FaceToolkit_PLATFORM}"
         BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/actual-build"
         INSTALL_DIR "${MODULE_INSTALL_PATH}"
         )
@@ -135,60 +135,60 @@ macro(PinscreenModulePrebuilt BASE_MODULE_NAME)
   endif()
   set(ACTUAL_BUILDING OFF)
   # add every child modules first
-  foreach(_module ${PSC_MODULE_DEPENDS})
+  foreach(_module ${FT_MODULE_DEPENDS})
     findModule(${_module})
   endforeach(_module)
 
   project(${BASE_MODULE_NAME})
   include("${MODULE_MANAGER_ROOT}/ccache/setup.cmake")
   # here follows the actual code
-endmacro(PinscreenModulePrebuilt)
+endmacro(FaceToolkitModulePrebuilt)
 
 
-macro(PinscreenModuleEnd BASE_MODULE_NAME)
+macro(FaceToolkitModuleEnd BASE_MODULE_NAME)
   set_target_properties(${BASE_MODULE_NAME} PROPERTIES
     POSITION_INDEPENDENT_CODE ON # always add fPIC
     COMPILE_OPTIONS "$<$<COMPILE_LANGUAGE:CXX>:-std=c++11>"
     XCODE_ATTRIBUTE_CC "${CMAKE_C_COMPILER}"
     XCODE_ATTRIBUTE_CXX "${CMAKE_CXX_COMPILER}")
 
-  add_library(PSM::${BASE_MODULE_NAME} ALIAS ${BASE_MODULE_NAME}) # attach PSM:: namespace for consistency when module is prebuilt
+  add_library(FTM::${BASE_MODULE_NAME} ALIAS ${BASE_MODULE_NAME}) # attach FTM:: namespace for consistency when module is prebuilt
 
   install(TARGETS ${BASE_MODULE_NAME}
     EXPORT ${BASE_MODULE_NAME}Targets
-    ARCHIVE DESTINATION lib/${PINSCREEN_OS_SUFFIX}
-    LIBRARY DESTINATION lib/${PINSCREEN_OS_SUFFIX}
+    ARCHIVE DESTINATION lib/${FaceToolkit_OS_SUFFIX}
+    LIBRARY DESTINATION lib/${FaceToolkit_OS_SUFFIX}
     INCLUDES DESTINATION include)
-  #install(EXPORT ${BASE_MODULE_NAME}Targets DESTINATION cmake/${PINSCREEN_OS_SUFFIX})
+  #install(EXPORT ${BASE_MODULE_NAME}Targets DESTINATION cmake/${FaceToolkit_OS_SUFFIX})
   set(PACKAGE_MODULE_NAME ${BASE_MODULE_NAME})
 
   configure_package_config_file(${MODULE_MANAGER_ROOT}/TemplateConfig.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/${BASE_MODULE_NAME}Config.cmake
     INSTALL_DESTINATION cmake)
   install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${BASE_MODULE_NAME}Config.cmake
     DESTINATION cmake)
-endmacro(PinscreenModuleEnd)
+endmacro(FaceToolkitModuleEnd)
 
-macro(PinscreenHeaderModule BASE_MODULE_NAME)
-  if(TARGET PSM::${BASE_MODULE_NAME})
+macro(FaceToolkitHeaderModule BASE_MODULE_NAME)
+  if(TARGET FTM::${BASE_MODULE_NAME})
     return()
   endif()
   set(optionArgs HIDE_HEADER)
   set(multiValueArgs DEPENDS)
   set(singleValueArgs LOCATION)
-  cmake_parse_arguments(PSC_MODULE "${optionArgs}" "${singleValueArgs}" "${multiValueArgs}" ${ARGN} )
-  if(NOT PSC_MODULE_LOCATION)
+  cmake_parse_arguments(FT_MODULE "${optionArgs}" "${singleValueArgs}" "${multiValueArgs}" ${ARGN} )
+  if(NOT FT_MODULE_LOCATION)
     message(FATAL_ERROR "need to specify when header files are.")
   endif()
-  PinscreenModuleBegin(${BASE_MODULE_NAME} DEPENDS ${PSC_MODULE_DEPENDS})
+  FaceToolkitModuleBegin(${BASE_MODULE_NAME} DEPENDS ${FT_MODULE_DEPENDS})
 
-  add_library(PSM::${BASE_MODULE_NAME} INTERFACE IMPORTED GLOBAL)
-  set_target_properties(PSM::${BASE_MODULE_NAME} PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${PSC_MODULE_LOCATION}"
+  add_library(FTM::${BASE_MODULE_NAME} INTERFACE IMPORTED GLOBAL)
+  set_target_properties(FTM::${BASE_MODULE_NAME} PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${FT_MODULE_LOCATION}"
   )
 
-  if(NOT PSC_MODULE_HIDE_HEADER)
+  if(NOT FT_MODULE_HIDE_HEADER)
     file(GLOB_RECURSE all_files "${CMAKE_CURRENT_LIST_DIR}/*.h" "${CMAKE_CURRENT_LIST_DIR}/*.hpp")
     add_custom_target(${BASE_MODULE_NAME} SOURCES ${all_files})
   endif()
 
-endmacro(PinscreenHeaderModule)
+endmacro(FaceToolkitHeaderModule)
